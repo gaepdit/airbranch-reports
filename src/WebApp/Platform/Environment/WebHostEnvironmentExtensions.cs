09e316b6ee89072e3654c087e822ec476a07b2be
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-11-23 11:58:34 -0500

    Set up local development environment

---
+++ src/WebApp/Platform/Environment/WebHostEnvironmentExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp.Platform.Environment;
+
+public static class WebHostEnvironmentExtensions
+{
+    public static bool IsLocalDev(this IWebHostEnvironment env) => env.IsEnvironment("Local");
+}
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,25 +1,36 @@
+using WebApp.Platform.Environment;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure the UI
 builder.Services.AddRazorPages();
 
+// Build the application
 var app = builder.Build();
+var env = app.Environment;
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (env.IsDevelopment() || env.IsLocalDev())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseStatusCodePages();
+}
+else
 {
     app.UseExceptionHandler("/Error");
+}
+
+if (!env.IsLocalDev())
+{
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapRazorPages();
-
 app.Run();
