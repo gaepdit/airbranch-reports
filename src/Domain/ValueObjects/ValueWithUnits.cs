7206e31ad98f0dec8ed6f9ec2bae11406a7b090e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-02-08 14:32:05 -0500

    Fix display of ValueWithUnits type in razor pages

--- src/Domain/ValueObjects/ValueWithUnits.cs
+++ src/Domain/ValueObjects/ValueWithUnits.cs
@@ -7,6 +7,6 @@ public record struct ValueWithUnits
     string? Preamble = null
 )
 {
-    public string Display() =>
+    public override string ToString() =>
         string.Join(" ", new[] { Preamble, Value, Units }.Where(s => !string.IsNullOrEmpty(s)));
 }
