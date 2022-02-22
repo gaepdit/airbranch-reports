ad28d62da0230ef22d7c77c5e038786deebbb8a4
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-02-22 15:22:07 -0500

    Cover additional ApbFacilityId unit tests

--- tests/DomainTests/Facilities/ApbFacilityIdTests/ApbFacilityIdObject.cs
+++ tests/DomainTests/Facilities/ApbFacilityIdTests/ApbFacilityIdObject.cs
@@ -23,6 +23,36 @@ public class ApbFacilityIdObject
     }
 
     [Test]
+    public void HasCorrectlyFormattedPropertiesFromLongForm()
+    {
+        var airs = new ApbFacilityId("041312345678");
+
+        Assert.Multiple(() =>
+        {
+            airs.ToString().Should().Be("123-45678");
+            airs.ShortString.Should().Be("12345678");
+            airs.FacilityId.Should().Be("123-45678");
+            airs.DbFormattedString.Should().Be("041312345678");
+            airs.EpaFacilityIdentifier.Should().Be("GA0000001312345678");
+        });
+    }
+
+    [Test]
+    public void HasCorrectlyFormattedPropertiesFromImplicitConversion()
+    {
+        ApbFacilityId airs = "041312345678";
+
+        Assert.Multiple(() =>
+        {
+            airs.ToString().Should().Be("123-45678");
+            airs.ShortString.Should().Be("12345678");
+            airs.FacilityId.Should().Be("123-45678");
+            airs.DbFormattedString.Should().Be("041312345678");
+            airs.EpaFacilityIdentifier.Should().Be("GA0000001312345678");
+        });
+    }
+
+    [Test]
     public void ThrowsOnInvalidAirsNumber()
     {
         var id = "abc";
--- tests/WebAppTests/Api/StackTest/GetStackTest.cs
+++ tests/WebAppTests/Api/StackTest/GetStackTest.cs
@@ -2,9 +2,11 @@ using Domain.Facilities.Models;
 using Domain.StackTest.Models;
 using Domain.StackTest.Repositories;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WebApp.Api.StackTest;
 
@@ -61,6 +63,85 @@ public class GetStackTest
             response.Result.Should().BeOfType<BadRequestResult>();
             var result = response.Result as BadRequestResult;
             result!.StatusCode.Should().Be(400);
+        });
+    }
+
+    [Test]
+    public async Task ConfidentialRequestReturnsOkIfUserLoggedIn()
+    {
+        var repo = new Mock<IStackTestRepository>();
+        repo.Setup(l => l.GetStackTestReportAsync(It.IsAny<ApbFacilityId>(), It.IsAny<int>()))
+            .ReturnsAsync(new StackTestReportOneStack());
+
+        var mockContext = new Mock<HttpContext>(MockBehavior.Strict);
+        mockContext.SetupGet(c => c.User).Returns(new ClaimsPrincipal());
+        mockContext.SetupGet(c => c.User.Identity!.IsAuthenticated).Returns(true);
+
+        var controller = new StackTestController
+        {
+            ControllerContext = new ControllerContext() { HttpContext = mockContext.Object }
+        };
+
+        var response = await controller.GetAsync(repo.Object, "00100001", default, true);
+
+        Assert.Multiple(() =>
+        {
+            response.Result.Should().BeOfType<OkObjectResult>();
+            var result = response.Result as OkObjectResult;
+            result!.StatusCode.Should().Be(200);
+            result.Value.Should().BeEquivalentTo(new StackTestReportOneStack());
+
+        });
+    }
+
+    [Test]
+    public async Task ConfidentialRequestReturnsChallengeIfUserIsNull()
+    {
+        var repo = new Mock<IStackTestRepository>();
+        repo.Setup(l => l.GetStackTestReportAsync(It.IsAny<ApbFacilityId>(), It.IsAny<int>()))
+            .ReturnsAsync(new StackTestReportOneStack());
+
+        var mockContext = new Mock<HttpContext>(MockBehavior.Strict);
+        mockContext.SetupGet(c => c.User.Identity).Returns(null as ClaimsIdentity);
+
+        var controller = new StackTestController
+        {
+            ControllerContext = new ControllerContext() { HttpContext = mockContext.Object }
+        };
+
+        var response = await controller.GetAsync(repo.Object, "00100001", default, true);
+
+        Assert.Multiple(() =>
+        {
+            response.Result.Should().BeOfType<ChallengeResult>();
+            var result = response.Result as ChallengeResult;
+            result!.AuthenticationSchemes.Should().BeEmpty();
+        });
+    }
+
+    [Test]
+    public async Task ConfidentialRequestReturnsChallengeIfUserNotAuthenticated()
+    {
+        var repo = new Mock<IStackTestRepository>();
+        repo.Setup(l => l.GetStackTestReportAsync(It.IsAny<ApbFacilityId>(), It.IsAny<int>()))
+            .ReturnsAsync(new StackTestReportOneStack());
+
+        var mockContext = new Mock<HttpContext>(MockBehavior.Strict);
+        mockContext.SetupGet(c => c.User).Returns(new ClaimsPrincipal());
+        mockContext.SetupGet(c => c.User.Identity!.IsAuthenticated).Returns(false);
+
+        var controller = new StackTestController
+        {
+            ControllerContext = new ControllerContext() { HttpContext = mockContext.Object }
+        };
+
+        var response = await controller.GetAsync(repo.Object, "00100001", default, true);
+
+        Assert.Multiple(() =>
+        {
+            response.Result.Should().BeOfType<ChallengeResult>();
+            var result = response.Result as ChallengeResult;
+            result!.AuthenticationSchemes.Should().BeEmpty();
         });
     }
 }
