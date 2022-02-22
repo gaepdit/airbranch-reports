3ea8660d073f39a2ffaae280639200028cd4b65c
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-02-22 13:01:58 -0500

    Replace Organization (Management) query with stored procedure

---
+++ src/Infrastructure/DB Objects/Organization/GetManagement.sql
@@ -0,0 +1,46 @@
+﻿USE airbranch;
+GO
+SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+CREATE OR ALTER PROCEDURE air.GetManagement
+    @Type varchar(20)
+AS
+
+/*******************************************************************************
+
+Author:     Doug Waldron
+Overview:   Retrieves the name of the current manager of the given type.
+
+Input Parameters:
+    @Type - The type of management (see allowable types)
+
+Allowable types:
+    DnrCommissioner
+    EpdDirector
+    ApbBranchChief
+    IsmpProgramManager
+    SscpProgramManager
+    SsppProgramManager
+
+Modification History:
+When        Who                 What
+----------  ------------------  ------------------------------------------------
+2022-02-22  DWaldron            Initial version
+
+*******************************************************************************/
+
+BEGIN
+    SET NOCOUNT ON;
+
+    select STRMANAGEMENTNAME
+    from LOOKUPAPBMANAGEMENTTYPE
+    where STRKEY = @Type
+      and STRCURRENTCONTACT = 'C';
+
+    return 0;
+END;
+
+GO
