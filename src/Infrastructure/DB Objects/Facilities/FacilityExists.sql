71f0252a5e7a2bd91f1b0c1b10dff45c141a39c5
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-12-08 16:28:26 -0500

    Update filter for approved facilities
    See gaepdit/iaip#1177

--- src/Infrastructure/DB Objects/Facilities/FacilityExists.sql
+++ src/Infrastructure/DB Objects/Facilities/FacilityExists.sql
@@ -2,14 +2,12 @@
 GO
 SET ANSI_NULLS ON;
 GO
-SET QUOTED_IDENTIFIER ON;
-GO
 
 CREATE OR ALTER PROCEDURE air.FacilityExists
     @AirsNumber varchar(12)
 AS
 
-/*******************************************************************************
+/**************************************************************************************************
 
 Author:     Doug Waldron
 Overview:   Reports whether facility with a given ID exists.
@@ -19,20 +17,17 @@ Input Parameters:
 
 Modification History:
 When        Who                 What
-----------  ------------------  ------------------------------------------------
+----------  ------------------  -------------------------------------------------------------------
 2022-02-22  DWaldron            Initial version
+2022-12-08  DWaldron            Update filter for approved facilities (IAIP-1177)
 
-*******************************************************************************/
+***************************************************************************************************/
 
 BEGIN
     SET NOCOUNT ON;
 
-    select convert(bit, count(*))
-    from dbo.AFSFACILITYDATA
-    where STRAIRSNUMBER = @AirsNumber
-      and STRUPDATESTATUS <> 'H'
+    SELECT iaip_facility.FacilityHasBeenApproved(@AirsNumber) as Result;
 
-    return 0;
 END;
 
 GO
