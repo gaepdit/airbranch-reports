2b853433d535d0b490d966a8d2e3741437d53d2c
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-10-02 11:56:06 -0400

    Fix sanitize.css SRI

--- src/WebApp/Platform/SecurityHeaders/SecurityHeaders.cs
+++ src/WebApp/Platform/SecurityHeaders/SecurityHeaders.cs
@@ -30,7 +30,7 @@ internal static class SecurityHeaders
             .ReportSample();
         builder.AddStyleSrc()
             .Self()
-            .From("https://cdn.jsdelivr.net/npm/sanitize.css@13.0.0/sanitize.min.css")
+            .From("https://cdn.jsdelivr.net/npm/sanitize.css@13.0.0/sanitize.css")
             .From("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css")
             .ReportSample();
         builder.AddImgSrc().Self();
